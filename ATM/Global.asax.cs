92d8466245f4e2b6520ac6a4bd26915ba500861d
David Moreira <david_nogueira_moreira@hotmail.com>
2018-07-29 19:08:20 +0100

    breaking changes

--- ATM/Global.asax.cs
+++ ATM/Global.asax.cs
@@ -2,57 +2,58 @@
 using ATM.Core.Interfaces;
 using ATM.Core.Interfaces.Services;
 using ATM.Core.Services;
-using ATM.Infrastructure.Data;
-using ATM.Infrastructure.Repositories;
+//using ATM.Infrastructure.Data;
+//using ATM.Infrastructure.Repositories;
 using Ninject;
 using Ninject.Web.Common.WebHost;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace ATM
 {
-    public class MvcApplication : NinjectHttpApplication
+    public class MvcApplication : HttpApplication
     {
-        //protected void Application_Start()
-        //{
-        //    AreaRegistration.RegisterAllAreas();
-        //    FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-        //    RouteConfig.RegisterRoutes(RouteTable.Routes);
-        //    BundleConfig.RegisterBundles(BundleTable.Bundles);
-        //}
-
-        protected override IKernel CreateKernel()
+        protected void Application_Start()
         {
-            var kernel = new StandardKernel();
-            RegisterServices(kernel);
-            return kernel;
-        }
-
-        /// <summary>
-        /// Load your modules or register your services here!
-        /// </summary>
-        /// <param name="kernel">The kernel.</param>
-        private void RegisterServices(IKernel kernel)
-        {
-            kernel.Bind(typeof(IRepositoryBase<>)).To(typeof(RepositoryBase<>));
-            kernel.Bind<IBankAccountRepo>().To<BankAccountRepo>();
-            kernel.Bind<ITransactionRepo>().To<TransactionRepo>();
-
-            kernel.Bind(typeof(IServiceBase<>)).To(typeof(ServiceBase<>));
-            kernel.Bind<IBankAccountService>().To<BankAccountManager>();
-            kernel.Bind<ITransactionService>().To<TransactionManager>();
-            kernel.Bind<IOperationService>().To<OperationManager>();
-        }
-
-        protected override void OnApplicationStarted()
-        {
-            base.OnApplicationStarted();
-
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        //protected override IKernel CreateKernel()
+        //{
+        //    var kernel = new StandardKernel();
+        //    RegisterServices(kernel);
+        //    return kernel;
+        //}
+
+        ///// <summary>
+        ///// Load your modules or register your services here!
+        ///// </summary>
+        ///// <param name="kernel">The kernel.</param>
+        //private void RegisterServices(IKernel kernel)
+        //{
+        //    //kernel.Bind(typeof(IRepositoryBase<>)).To(typeof(RepositoryBase<>));
+        //    //kernel.Bind<IBankAccountRepo>().To<BankAccountRepo>();
+        //    //kernel.Bind<ITransactionRepo>().To<TransactionRepo>();
+
+        //    kernel.Bind(typeof(IServiceBase<>)).To(typeof(ServiceBase<>));
+        //    kernel.Bind<IBankAccountService>().To<BankAccountManager>();
+        //    kernel.Bind<ITransactionService>().To<TransactionManager>();
+        //    kernel.Bind<IOperationService>().To<OperationManager>();
+        //}
+
+        //protected override void OnApplicationStarted()
+        //{
+        //    base.OnApplicationStarted();
+
+        //    AreaRegistration.RegisterAllAreas();
+        //    FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+        //    RouteConfig.RegisterRoutes(RouteTable.Routes);
+        //    BundleConfig.RegisterBundles(BundleTable.Bundles);
+        //}
     }
 }
