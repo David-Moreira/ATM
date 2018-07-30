809058851482071f65cc1d5d86dd307c501446c3
David Moreira <david_nogueira_moreira@hotmail.com>
2018-07-31 00:42:52 +0100

    Error Validation / Handling in place / Some Improvements / Still in development

---
+++ ATM.Core/Interfaces/Validation/IOperationValidator.cs
@@ -0,0 +1,11 @@
+﻿using ATM.Core.Validation;
+
+namespace ATM.Core.Interfaces.Validation
+{
+    public interface IOperationValidator
+    {
+        OperationResult ValidateUserAccount(string userID, int accountNumber);
+        OperationResult ValidateAmount(int amount);
+        OperationResult ValidateAmountForPayment(int balance, int amountToSubtract);
+    }
+}
